63c70aa14c3600ea6fe8cf8010bedde7b79d9ea0
iguanajuice <125163000+iguanajuice@users.noreply.github.com>
2023-06-01 18:31:52 -0400

    Update debian.sh

--- install/debian.sh
+++ install/debian.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo apt install \
-sway swaybg swaylock waybar mako fuzzel foot micro playerctl light policykit-1-gnome xdg-desktop-portal-wlr gnome-calculator gnome-calendar network-manager-gnome copyq wl-clipboard slurp grim
+sway swaybg swaylock waybar mako fuzzel foot micro playerctl light policykit-1-gnome xdg-desktop-portal-wlr gnome-calculator gnome-calendar gnome-system-monitor network-manager-gnome copyq wl-clipboard slurp grim
 
 cp -r config/* ~/.config
 chmod -R +x ~/.config/sway/bin
