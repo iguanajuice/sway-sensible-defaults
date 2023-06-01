5b7bdc12cb0df86d68d4a857f0df30461a042eef
iguanajuice <125163000+iguanajuice@users.noreply.github.com>
2023-06-01 01:03:04 -0400

    chmod bin files in debian.sh

--- install/debian.sh
+++ install/debian.sh
@@ -4,3 +4,4 @@ sudo apt install \
 sway swaybg swaylock waybar mako fuzzel foot micro playerctl light policykit-1-gnome xdg-desktop-portal-wlr gnome-calculator gnome-calendar network-manager-gnome copyq wl-clipboard slurp grim
 
 cp -r config/* ~/.config
+chmod -R +x ~/.config/sway/bin
