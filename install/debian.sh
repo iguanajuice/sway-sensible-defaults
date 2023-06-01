7279da754324b122ba8088c42d95811090e8a1e3
iguanajuice <125163000+iguanajuice@users.noreply.github.com>
2023-06-01 00:56:24 -0400

    Add packages wl-clipboard, slurp, and grim debian.sh

--- install/debian.sh
+++ install/debian.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 sudo apt install \
-sway swaybg swaylock waybar mako fuzzel foot micro playerctl light policykit-1-gnome xdg-desktop-portal-wlr gnome-calculator gnome-calendar network-manager-gnome copyq
+sway swaybg swaylock waybar mako fuzzel foot micro playerctl light policykit-1-gnome xdg-desktop-portal-wlr gnome-calculator gnome-calendar network-manager-gnome copyq wl-clipboard slurp grim
 
 cp -r config/* ~/.config
