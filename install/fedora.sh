fed6532ca7b1424324cdd4ec6e7455a160c70369
iguanajuice <125163000+iguanajuice@users.noreply.github.com>
2023-06-01 00:56:54 -0400

    Add packages wl-clipboard, slurp, and grim fedora.sh

--- install/fedora.sh
+++ install/fedora.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 sudo dnf install -y \
-sway swaybg swaylock waybar mako fuzzel foot micro playerctl light polkit-gnome xdg-desktop-portal-wlr gnome-calculator gnome-calendar network-manager-applet copyq
+sway swaybg swaylock waybar mako fuzzel foot micro playerctl light polkit-gnome xdg-desktop-portal-wlr gnome-calculator gnome-calendar network-manager-applet copyq wl-clipboard slurp grim
 
 cp -r config/* ~/.config
