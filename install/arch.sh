25808a7915f8995a86cfbe52804aa318edda6394
iguanajuice <125163000+iguanajuice@users.noreply.github.com>
2023-06-01 01:02:44 -0400

    chmod bin files in arch.sh

--- install/arch.sh
+++ install/arch.sh
@@ -4,3 +4,4 @@ sudo pacman --needed --noconfirm -S \
 sway swaybg swaylock waybar mako fuzzel foot micro playerctl light polkit-gnome xdg-desktop-portal-wlr gnome-calculator gnome-calendar network-manager-applet copyq wl-clipboard slurp grim
 
 cp -r config/* ~/.config
+chmod -R +x ~/.config/sway/bin
