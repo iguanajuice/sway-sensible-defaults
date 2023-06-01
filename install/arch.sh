3d51ee0c38be17a947c695af203606cf578cac8b
iguanajuice <125163000+iguanajuice@users.noreply.github.com>
2023-06-01 00:55:46 -0400

    Add packages wl-clipboard, slurp, and grim to arch.sh

--- install/arch.sh
+++ install/arch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 sudo pacman --needed --noconfirm -S \
-sway swaybg swaylock waybar mako fuzzel foot micro playerctl light polkit-gnome xdg-desktop-portal-wlr gnome-calculator gnome-calendar network-manager-applet copyq
+sway swaybg swaylock waybar mako fuzzel foot micro playerctl light polkit-gnome xdg-desktop-portal-wlr gnome-calculator gnome-calendar network-manager-applet copyq wl-clipboard slurp grim
 
 cp -r config/* ~/.config
