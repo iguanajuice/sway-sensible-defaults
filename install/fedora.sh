8de73312c1a58c4ce734fc45d2e1116d400ea4dd
iguanajuice <125163000+iguanajuice@users.noreply.github.com>
2023-06-03 18:16:41 -0400

    Update fedora.sh

--- install/fedora.sh
+++ install/fedora.sh
@@ -3,7 +3,7 @@
 sudo dnf install -y \
 sway swaybg swaylock waybar mako fuzzel foot micro playerctl light polkit-gnome xdg-desktop-portal-wlr \
 gnome-calculator gnome-calendar gnome-system-monitor gnome-tweaks \
-network-manager-applet copyq wl-clipboard slurp grim
+network-manager-applet pavucontrol copyq wl-clipboard slurp grim
 
 cp -r config/* ~/.config
 chmod -R +x ~/.config/sway/bin
