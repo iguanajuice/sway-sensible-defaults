36e253fa1dc56ae29b5b34ae74511d438b79de3b
iguanajuice <125163000+iguanajuice@users.noreply.github.com>
2023-06-03 18:04:25 -0400

    Delete debian.sh

--- install/debian.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sudo apt install \
-sway swaybg swaylock waybar mako-notifier wofi foot micro playerctl light policykit-1-gnome xdg-desktop-portal-wlr gnome-calculator gnome-calendar gnome-system-monitor network-manager-gnome copyq wl-clipboard slurp grim
-
-cp -r config/* ~/.config
-chmod -R +x ~/.config/sway/bin
-sudo cp -r share/* /usr/share
