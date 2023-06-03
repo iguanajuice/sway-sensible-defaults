e23f8c72badbabd7e395ee57f45531e360697589
iguanajuice <125163000+iguanajuice@users.noreply.github.com>
2023-06-03 18:00:38 -0400

    Update arch.sh

--- install/arch.sh
+++ install/arch.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 sudo pacman --needed --noconfirm -S \
-sway swaybg swaylock waybar mako fuzzel foot micro playerctl light polkit-gnome xdg-desktop-portal-wlr gnome-calculator gnome-calendar gnome-system-monitor network-manager-applet copyq wl-clipboard slurp grim
+sway swaybg swaylock waybar mako fuzzel foot micro playerctl light polkit-gnome xdg-desktop-portal-wlr \
+gnome-calculator gnome-calendar gnome-system-monitor gnome-tweaks qgnomeplatform-qt5 qgnomeplatform-qt6 \
+network-manager-applet copyq wl-clipboard slurp grim
 
 cp -r config/* ~/.config
 chmod -R +x ~/.config/sway/bin
