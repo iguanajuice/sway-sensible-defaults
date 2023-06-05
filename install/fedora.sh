cbdf422433b4623be79a7828c7703dbd4eabb00c
iguanajuice <125163000+iguanajuice@users.noreply.github.com>
2023-06-05 03:14:24 -0400

    Update fedora.sh

--- install/fedora.sh
+++ install/fedora.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo dnf install -y \
-sway swaybg swaylock waybar mako fuzzel foot micro playerctl light polkit-gnome xdg-desktop-portal-wlr \
+sway swaybg swaylock waybar mako fuzzel kitty micro playerctl light polkit-gnome xdg-desktop-portal-wlr \
 gnome-calculator gnome-calendar gnome-system-monitor gnome-tweaks \
 network-manager-applet pavucontrol copyq wl-clipboard slurp grim
 
