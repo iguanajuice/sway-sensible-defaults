a5630960ea02b7f5d6df28b8c9a532251f2af042
iguanajuice <125163000+iguanajuice@users.noreply.github.com>
2023-06-07 15:06:06 -0400

    Update arch.sh

--- install/arch.sh
+++ install/arch.sh
@@ -3,10 +3,10 @@
 sudo pacman --needed --noconfirm -S \
 sway swaybg swaylock waybar mako fuzzel kitty micro playerctl light polkit-gnome xdg-desktop-portal-wlr \
 gnome-calculator gnome-calendar gnome-system-monitor gnome-tweaks gnome-themes-extra qgnomeplatform-qt5 qgnomeplatform-qt6 \
-network-manager-applet pavucontrol copyq wl-clipboard slurp grim gsfonts otf-font-awesome
+dex network-manager-applet pavucontrol copyq wl-clipboard slurp grim gsfonts otf-font-awesome
 
 cp -r config/* ~/.config
 chmod -R +x ~/.config/sway/bin
-sudo cp -r share/* /usr/share
+sudo cp share/icons/nwg-bar /usr/share/icons
 echo 'QT_QPA_PLATFORMTHEME=gnome /bin/sway -D noscanout "$@"' | sudo tee /usr/local/bin/sway
 sudo chmod +x /usr/local/bin/sway
