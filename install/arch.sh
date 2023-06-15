2497d54b04976958a47d1d476b44c16d3006dc4c
iguanajuice <125163000+iguanajuice@users.noreply.github.com>
2023-06-14 20:16:46 -0400

    Update arch.sh

--- install/arch.sh
+++ install/arch.sh
@@ -3,7 +3,7 @@
 sudo pacman --needed --noconfirm -S \
 sway swaybg swaylock waybar mako fuzzel kitty micro playerctl light polkit-gnome xdg-desktop-portal-wlr xorg-xwayland \
 gnome-calculator gnome-calendar gnome-weather gnome-system-monitor gnome-tweaks gnome-themes-extra
-qgnomeplatform-qt5 qgnomeplatform-qt6 dex nm-connection-editor pavucontrol copyq wl-clipboard slurp grim gsfonts otf-font-awesome
+qgnomeplatform-qt5 qgnomeplatform-qt6 dex nm-connection-editor pavucontrol copyq wl-clipboard slurp grim gsfonts ttf-font-awesome
 
 cp -r config/* ~/.config
 chmod -R +x ~/.config/sway/bin
