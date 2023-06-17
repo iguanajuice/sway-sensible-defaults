0e312d5ce2383fac4ac906b42e34e18112c691a9
iguanajuice <125163000+iguanajuice@users.noreply.github.com>
2023-06-17 17:43:49 -0400

    Update arch.sh

--- install/arch.sh
+++ install/arch.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
-sudo pacman --needed --noconfirm -S \
-sway swaybg swaylock waybar mako fuzzel kitty micro playerctl light polkit-gnome xdg-desktop-portal-wlr xorg-xwayland \
-gnome-calculator gnome-calendar gnome-weather gnome-system-monitor gnome-tweaks gnome-themes-extra \
-qgnomeplatform-qt5 qgnomeplatform-qt6 dex nm-connection-editor pavucontrol copyq wl-clipboard slurp grim gsfonts ttf-font-awesome
-
-cp -r config/* ~/.config
-chmod -R +x ~/.config/sway/bin
-sudo cp -r share/icons/nwg-bar /usr/share/icons
-echo 'XDG_CURRENT_DESKTOP=sway QT_QPA_PLATFORMTHEME=gnome /bin/sway -D noscanout "$@"' | sudo tee /usr/local/bin/sway
-sudo chmod +x /usr/local/bin/sway
+sudo sh -c "
+  pacman --needed --noconfirm -S \
+  sway swaybg swaylock waybar mako fuzzel kitty micro playerctl light polkit-gnome xdg-desktop-portal-wlr xorg-xwayland \
+  gnome-calculator gnome-calendar gnome-weather gnome-system-monitor gnome-tweaks gnome-themes-extra \
+  qgnomeplatform-qt5 qgnomeplatform-qt6 dex nm-connection-editor pavucontrol copyq wl-clipboard slurp grim gsfonts ttf-font-awesome
+  
+  cp -r config/* ~/.config
+  chmod -R +x ~/.config/sway/bin
+  cp -r share/icons/nwg-bar /usr/share/icons
+  echo 'XDG_CURRENT_DESKTOP=sway QT_QPA_PLATFORMTHEME=gnome /bin/sway -D noscanout "$@"' | sudo tee /usr/local/bin/sway
+  chmod +x /usr/local/bin/sway
+  
+  mv /usr/share/applications/kitty-open.desktop /usr/share/applications/kitty-open.desktop.bak
+"
 
 gsettings set org.cinnamon.desktop.default-applications.terminal exec kitty
 gsettings set org.gnome.desktop.interface clock-format 12h
